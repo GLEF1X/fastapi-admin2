338a4f500cb5e735817092504409f32c0029822f
GLEF1X <glebgar567@gmail.com>
2022-04-06 23:55:09 +0300

    improve localization: remove flags, add autodetection of language name

--- fastapi_admin2/localization/middleware.py
+++ fastapi_admin2/localization/middleware.py
@@ -1,7 +1,8 @@
 import functools
 from abc import ABC, abstractmethod
-from typing import cast, Optional
+from typing import cast, Optional, List, Protocol
 
+import pycountry
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
@@ -21,6 +22,13 @@ except ImportError:  # pragma: no cover
     Locale = None
 
 
+class Language(Protocol):
+    alpha_3: str
+    name: str
+    scope: str
+    type: str
+
+
 class AbstractI18nMiddleware(BaseHTTPMiddleware, ABC):
 
     def __init__(self, app: ASGIApp,
@@ -30,7 +38,8 @@ class AbstractI18nMiddleware(BaseHTTPMiddleware, ABC):
         if translator is None:
             self._translator = I18nTranslator()
 
-        self._gettext_callables_were_installed = False
+        self._languages = pycountry.languages
+        self._lang_iterator = iter(self._languages)  # avoid pycountry lazy loading
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         current_locale = await self.get_locale(request)
@@ -38,11 +47,27 @@ class AbstractI18nMiddleware(BaseHTTPMiddleware, ABC):
         request.state.lazy_gettext = functools.partial(self._translator.lazy_gettext, locale=current_locale)
         request.state.current_locale = current_locale
 
+        request.app.templates.env.globals['gettext'] = request.state.gettext
+        request.app.templates.env.globals['current_locale'] = current_locale
+        request.app.templates.env.globals['available_languages'] = list(self.iter_founded_locales())
+
         with self._translator.internationalized(new_locale=current_locale):
             response = await call_next(request)
-        response.set_cookie(key="language", value=current_locale, path=request.app.admin_path)
 
+        response.set_cookie(key="language", value=current_locale, path=request.app.admin_path)
         return response
+
+    def iter_founded_locales(self) -> List[Language]:
+        for t in self._translator.available_translations:
+            try:
+                lang = self._languages.get(alpha_2=t)
+            except LookupError:
+                continue
+
+            if lang is None:
+                continue
+
+            yield lang
 
     @abstractmethod
     async def get_locale(self, request: Request) -> str:
@@ -74,7 +99,7 @@ class I18nMiddleware(AbstractI18nMiddleware):
             return self._translator.default_locale
 
         parsed_locale = Locale.parse(locale)
-        if parsed_locale.language not in self._translator.available_locales:
+        if parsed_locale.language not in self._translator.available_translations:
             return self._translator.default_locale
         return cast(str, parsed_locale.language)
 
